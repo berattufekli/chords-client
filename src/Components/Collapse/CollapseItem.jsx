725d30ea60a0ca2bbcfd1c885d546a1b5584c68b
la-berat <123157080+la-berat@users.noreply.github.com>
2023-08-19 23:33:35 +0300

    google analytics

--- src/Components/Collapse/CollapseItem.jsx
+++ src/Components/Collapse/CollapseItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import ReactGA from "react-ga";
 
 function turkishtoEnglish(input) {
   return input
@@ -19,26 +20,35 @@ function turkishtoEnglish(input) {
 }
 
 function CollapseItem({ item }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    ReactGA.event({
+      category: item.songName,
+      action: "Click",
+      label: "Şarkıya Girdi",
+      value: item.songId,
+    })
+    navigate(`/akor/${turkishtoEnglish(item.artistInfo.artistName.split(" ").join("-").toLowerCase())}/${turkishtoEnglish(item.songName.split(" ").join("-").toLowerCase())}/${item.songId}`);
+  }
+
   return (
-    <Link to={
-      `/akor/${turkishtoEnglish(item.artistInfo.artistName.split(" ").join("-").toLowerCase())}/${turkishtoEnglish(item.songName.split(" ").join("-").toLowerCase())}/${item.songId}`}>
-      <div className="group relative flex gap-x-6 rounded-lg p-4 items-center transition-all hover:bg-indigo-100">
-        <div className="flex overflow-hidden w-12 h-12 flex-none items-center justify-center rounded-lg">
-          <img
-            className="inline-block transition-all duration-500 group-hover:scale-125"
-            src={item.artistInfo.url}
-            alt=""
-          />
-        </div>
-        <div>
-          <p href={item.href} className="font-bold text-md text-gray-700">
-            {item.songName}
-            <span className="absolute inset-0" />
-          </p>
-          <p className=" text-gray-500">{item.artistInfo.artistName}</p>
-        </div>
+    <div onClick={handleClick} className="group relative flex gap-x-6 rounded-lg p-4 items-center transition-all hover:bg-indigo-100">
+      <div className="flex overflow-hidden w-12 h-12 flex-none items-center justify-center rounded-lg">
+        <img
+          className="inline-block transition-all duration-500 group-hover:scale-125"
+          src={item.artistInfo.url}
+          alt=""
+        />
       </div>
-    </Link>
+      <div>
+        <p href={item.href} className="font-bold text-md text-gray-700">
+          {item.songName}
+          <span className="absolute inset-0" />
+        </p>
+        <p className=" text-gray-500">{item.artistInfo.artistName}</p>
+      </div>
+    </div>
   )
 }
 
